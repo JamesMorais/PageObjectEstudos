8272a3fa1c3bc340ed1f09c5badef08bed479998
JamesMorais <capic.2022118tads0048@aluno.ifpi.edu.br>
2024-01-23 21:37:28 -0300

    implementado novos métodos

--- src/test/java/Home.java
+++ src/test/java/Home.java
@@ -1,6 +1,8 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
 public class Home {
+    private WebDriver driver;
     //Preciso criar a classe para cada página
     //tudo o que eu for testar eu preciso criar o campo aqui.
     //No caso se eu for fazer uma busca(terei que por o campo de pesquisa)
@@ -15,11 +17,20 @@ public class Home {
     By botaoPesquisa = By.xpath("//i[@class='la la-search' and @aria-hidden='true']");
 
     // Inserir nome de produto que será pesquisado
-    public void inserirProdutoCampoPesquisa(){
 
+    public void inserirProdutoCampoPesquisa(String nomeDoProdutoParaBuscar){
+        driver.findElement(campoPesquisa).sendKeys(nomeDoProdutoParaBuscar);
     }
 
 
     // Clicar no botão de pesquisa
-    public void clicarBotaoPesquisar(){}
+    public void clicarBotaoPesquisar(){
+        driver.findElement(botaoPesquisa).click();
+    }
+
+    //Construtor
+
+    public Home(WebDriver driver) {
+        this.driver = driver;
+    }
 }
--- src/test/java/TestaPaginas.java
+++ src/test/java/TestaPaginas.java
@@ -1,19 +1,40 @@
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class TestaPaginas {
     private WebDriver driver;
 
     private static final String baseUrl = "https://automacao.testerglobal.com/";
+    Home homePage;
+    @Test
+    @DisplayName("Quando pesquisa pesquisar produto por nome, então deve ser mostrada página do produto")
+    public void pesquisaProdutoPorNome(){
+        //Entrar no site de teste
+        driver.get(baseUrl);
+        //Instanciar o Page object
+        homePage = new Home(driver);
+        // Realizar a busca por um produto
+        String nomeProduto = "Camera";
+        homePage.inserirProdutoCampoPesquisa(nomeProduto);
+
+        // Clicar no botao de busca
+        homePage.clicarBotaoPesquisar();
+
+        String tituloPaginaEsperada = "Camera \\u2013 Praticar Automa\\u00E7\\u00E3o CTG \\u2013 ESTUDOS";
+        String tituloPaginaEncontrada = driver.getTitle();
+        assertEquals(tituloPaginaEsperada, tituloPaginaEncontrada);
+    }
     @Test
     @DisplayName("Quando clicar no botão pesquisar sem inserir nada então devo ver todos os produtos")
     public void pesquisarCampoVazio(){
+        driver.get(baseUrl);
+        homePage = new Home(driver);
         //Entrar na Pagina de testes
 
         // Não inseriu nada no campo pesquisa
@@ -35,7 +56,7 @@ public class TestaPaginas {
     //Precisa por uma anotação também
     @AfterAll
     public void tearDown(){
-        driver.close();
+        driver.quit();
     }
 
 }
